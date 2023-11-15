a8169f805aa19e7a1b88426561b7900ccc27a3ed
nicepowpowloko <90335368+nicepowpowloko@users.noreply.github.com>
2023-11-15 09:47:08 +0100

    15/11/2023

--- JAVASCRIPT/menu_animer.js
+++ JAVASCRIPT/menu_animer.js
@@ -14,8 +14,8 @@ menuButton.addEventListener("click", () => {
 
 //À l'intérieur de la fonction callback () => { ... }, vous avez deux actions qui sont exécutées lors du clic sur le bouton :
 
-    //menuButton.classList.toggle("active"); : Cette ligne de code ajoute ou supprime la classe CSS "active" à l'élément menuButton. La classe "active" est utilisée pour effectuer une rotation sur le bouton et lui donner l'apparence d'un X (ou d'un signe de fermeture) lorsque le menu est ouvert.
+//menuButton.classList.toggle("active"); : Cette ligne de code ajoute ou supprime la classe CSS "active" à l'élément menuButton. La classe "active" est utilisée pour effectuer une rotation sur le bouton et lui donner l'apparence d'un X (ou d'un signe de fermeture) lorsque le menu est ouvert.
 
-   // menuItems.classList.toggle("active"); : Cette ligne de code ajoute ou supprime la classe CSS "active" à l'élément menuItems. La classe "active" est utilisée pour afficher ou masquer les éléments du menu. Lorsqu'elle est ajoutée, les éléments du menu s'affichent en forme de cercle, et lorsqu'elle est supprimée, le menu est réduit et les éléments sont masqués.
+// menuItems.classList.toggle("active"); : Cette ligne de code ajoute ou supprime la classe CSS "active" à l'élément menuItems. La classe "active" est utilisée pour afficher ou masquer les éléments du menu. Lorsqu'elle est ajoutée, les éléments du menu s'affichent en forme de cercle, et lorsqu'elle est supprimée, le menu est réduit et les éléments sont masqués.
 
 //En résumé, ce code JavaScript permet de basculer les classes "active" sur le bouton et les éléments du menu lorsque le bouton est cliqué, ce qui déclenche l'animation d'ouverture et de fermeture du menu circulaire.
